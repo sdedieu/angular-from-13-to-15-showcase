ee0d08b2e75c603d6260cc7afc55e9ab4dd818df
s.dedieu <s.dedieu@criteo.com>
2023-05-05 14:37:29 +0200

    Performed 3rd migration part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { LoadPlanets } from './+state/galaxy.state';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common';
+import { MatLegacyListModule } from '@angular/material/legacy-list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatLegacyButtonModule } from '@angular/material/legacy-button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: true,
+    imports: [MatToolbarModule, MatLegacyButtonModule, MatIconModule, MatSidenavModule, MatLegacyListModule, NgFor, RouterLink, RouterOutlet]
 })
 export class AppComponent {
 
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,32 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { GalaxyState } from './app/+state/galaxy.state';
+import { NgxsModule } from '@ngxs/store';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
+import { MatLegacyListModule as MatListModule } from '@angular/material/legacy-list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { AppRoutingModule } from './app/app-routing.module';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule, MatIconModule, MatToolbarModule, MatSidenavModule, MatListModule, MatButtonModule, NgxsModule.forRoot([GalaxyState], {
+            developmentMode: !environment.production
+        })),
+        provideAnimations(),
+        provideHttpClient(withInterceptorsFromDi())
+    ]
+})
   .catch(err => console.error(err));
